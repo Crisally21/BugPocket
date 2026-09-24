80a55aae30944953b6340f059afe6f5925cb09df
Владимир <vovaelandaev@gmail.com>
2026-09-24 12:14:47 +0300

    feat: classify attachment formats as images or videos

--- src/main/java/ru/codex/codextest/model/AttachmentFileType.java
+++ src/main/java/ru/codex/codextest/model/AttachmentFileType.java
@@ -1,2 +1,17 @@
 package ru.codex.codextest.model;
-public enum AttachmentFileType { PNG, JPG, JPEG, MP4, MOV, MKV, AVI, WEBM }
+
+public enum AttachmentFileType {
+    PNG, JPG, JPEG, MP4, MOV, MKV, AVI, WEBM;
+
+    public AttachmentMediaKind getMediaKind() {
+        switch (this) {
+            case JPG, PNG, JPEG -> {
+                return AttachmentMediaKind.IMAGE;
+            }
+            default -> {
+                return AttachmentMediaKind.VIDEO;
+            }
+
+        }
+    }
+}
