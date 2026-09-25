b0cb711ffbc0ec912104e6b929d3c4034014979f
Владимир <vovaelandaev@gmail.com>
2026-09-25 10:51:27 +0300

    test: reject attachments one byte above size limit

--- src/test/java/ru/codex/codextest/AttachmentValidatorTests.java
+++ src/test/java/ru/codex/codextest/AttachmentValidatorTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import ru.codex.codextest.service.AttachmentValidator;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class AttachmentValidatorTests {
 
     @Test
@@ -11,5 +13,14 @@ public class AttachmentValidatorTests {
         AttachmentValidator validator = new AttachmentValidator();
         var file = new MockMultipartFile("file", "screen.png", "image/png", new byte[25000000]);
         validator.validateSize(file);
+    }
+
+    @Test
+    void rejectsFileAboveSizeLimit() {
+        AttachmentValidator validator = new AttachmentValidator();
+        var file = new MockMultipartFile("file", "screen.png", "image/png", new byte[25000001]);
+        assertThatThrownBy(() -> validator.validateSize(file))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Размер файла превышает 25 MB");
     }
 }
