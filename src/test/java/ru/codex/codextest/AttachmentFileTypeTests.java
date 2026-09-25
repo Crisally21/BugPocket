cc8d6ebbd1bdb415a81bd65c8750ee1d25e54705
Владимир <vovaelandaev@gmail.com>
2026-09-25 18:54:21 +0300

    test: parse attachment type from filename

--- src/test/java/ru/codex/codextest/AttachmentFileTypeTests.java
+++ src/test/java/ru/codex/codextest/AttachmentFileTypeTests.java
@@ -12,4 +12,10 @@ public class AttachmentFileTypeTests {
         assertThat(AttachmentFileType.fromExtension("jpg"))
                 .isEqualTo(AttachmentFileType.JPG);
     }
+
+    @Test
+    void fromFileNameTest() {
+        assertThat(AttachmentFileType.fromFilename("photo.jpg"))
+                .isEqualTo(AttachmentFileType.JPG);
+    }
 }
